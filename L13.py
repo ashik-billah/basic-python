8c1993bc4a6ddc7de771ddd67d38f4138d9deb72
ashik-billah <ashik.210101@s.pust.ac.bd>
2025-10-23 13:06:28 +0600

    print 1 t0 9 trangle

---
+++ L13.py
@@ -0,0 +1,11 @@
+for i in range(1,10):
+    for j in range(1,i+1):
+        print(j,end=' ')
+    print()
+    
+    # anoth
+    
+for i in range(1, 10):
+    for j in range(10, i-1, -1):   # step -1
+        print('*', end=' ')
+    print()
