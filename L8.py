922889b13bd80150481df0aa7128b67ff9f2e376
ashik-billah <ashik.210101@s.pust.ac.bd>
2025-10-23 11:23:18 +0600

    check character vowel or consonants

---
+++ L8.py
@@ -0,0 +1,12 @@
+char = input('Enter the letter')
+match char:
+    case "a":
+        print(char,'is a vowel')
+    case "e":
+        print(char,'is a vowel')
+    case "i":
+        print(char,'is a vowel')
+    case "u":
+        print(char,'is a vowel')
+    case _:
+        print(char,'is a comsonant')
