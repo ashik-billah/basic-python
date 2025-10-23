0e9f17a79c314fc5bcac2b50812ac3d00992df17
ashik-billah <ashik.210101@s.pust.ac.bd>
2025-10-23 12:15:36 +0600

    print 5 word

---
+++ L10.py
@@ -0,0 +1,20 @@
+for i in range (1,6):
+    print('ICT')
+
+print('\n')
+
+#print in  one line
+for i in range(1, 6):
+    print('ICT', end=' ')
+
+print('\n')
+
+#another logic
+for i in range (1,6,2):
+    print('ICT')
+    
+print('\n')
+
+#another logic
+for i in range (6,1,-1):
+    print('ICT')
