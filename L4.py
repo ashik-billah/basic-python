63d51bea9c6f24937cd642acee28bf9505ecfce6
ashik-billah <ashik.210101@s.pust.ac.bd>
2025-10-23 10:43:55 +0600

    detect even odd number

---
+++ L4.py
@@ -0,0 +1,13 @@
+num = int(input('Insert a number'))
+if num%2!=0:
+    print('The number is Odd number')
+else:
+    print('The number is Even number')
+    
+    
+    #another method
+    num = int(input('Insert a number'))
+if num%2==0:
+    print('The number is Even number')
+else:
+    print('The number is Odd number')
